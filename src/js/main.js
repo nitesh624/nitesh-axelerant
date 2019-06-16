2c5891ff7080bcbc1dc9dfa4d877b166922c58d3
nitesh624 <nsinha624@gmail.com>
2019-06-16 12:37:53 +0530

    changes in theme

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('.toggle-menu').click(function() {
     $('ul').toggleClass('active');
   });
+
+  $('.toggle-menu-2').click(function() {
+    $('nav__links').toggleClass('active-2');
+  });
 });
